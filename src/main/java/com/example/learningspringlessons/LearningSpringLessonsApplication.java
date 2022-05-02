7b8d72872df4a6d294342fbd925e0f80dc638c8b
zephir0 <krystiannzajac@gmail.com>
2022-05-02 01:41:58 +0100

    first spring project

---
+++ src/main/java/com/example/learningspringlessons/LearningSpringLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningspringlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringLessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringLessonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningspringlessons/LearningSpringLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningspringlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringLessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
